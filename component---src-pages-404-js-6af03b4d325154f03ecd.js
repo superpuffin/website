94055a2add703fb1945d6286344941ed89f66913
Yorgos Bos <yorgos.bos@outlook.com>
2020-12-29 19:14:26 +0100

    Updates

---
+++ component---src-pages-404-js-6af03b4d325154f03ecd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(t,e,a){"use strict";a.r(e);var n=a("q1tI"),l=a.n(n),o=a("Bl7J"),i=a("vrFN");e.default=function(t){var e=t.data,a=(t.location,e.site.siteMetadata.title);return l.a.createElement(o.a,{title:a},l.a.createElement(i.a,{title:"404: Not Found"}),l.a.createElement("h1",null,"404: Not Found"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6af03b4d325154f03ecd.js.map

aa19d305cf8e9e843f99197c35afa5953615fde8
Yorgos Bos <yorgos.bos@outlook.com>
2020-12-30 15:51:24 +0100

    Style svg with classes

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,12 @@ const Layout = ({ children }) => {
       <header className="">
         {/* {header} */}
         <Nav className="bg-primary" />
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <svg
+          className="fill-current text-primary"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+        >
           <path
-            fill="#00A7E1"
             fill-opacity="1"
             d="M0,256L48,229.3C96,203,192,149,288,138.7C384,128,480,160,576,144C672,128,768,64,864,69.3C960,75,1056,149,1152,176C1248,203,1344,181,1392,170.7L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
           ></path>
